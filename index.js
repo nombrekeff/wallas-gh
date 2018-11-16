d915d759978d46be1d6a27bdd5f4c79dbf82e088
Mona Lisa <manoloedge96@gmail.com>
2018-11-16 17:11:58 +0100

    started testing

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ module.exports = app => {
   // app.log('Yay, the app was loaded!')
 
   app.on('issues.opened', async context => {
-    app.log('Issue opened')
+    app.log('Issue opened');
 
-    const comment = context.issue({ body: 'Thanks for opening this issue! \nWe will check it ASAP.' })
-    // const labels = context.issue({ labels: [{ name: '' }] })
+    const comment = context.issue({ body: 'Thanks for opening this issue! \nWe will check it ASAP.' });
 
-    context.github.issues.createComment(comment)
-    context.github.issues.addLabels(labels)
+    context.github.issues.createComment(comment);
+    context.github.issues.addLabels(labels);
   })
 
-  app.on('push', async context => {
-    app.log('on push', context)
-  })
+  app.on('create', async context => {
+    if (context.payload.ref_type !== 'tag') return;
+
+    app.log('Tag created: ', context.payload.ref);
+    const label = context.issue({
+      name: context.payload.ref,
+      description: 'A label for issues related to version/tag: ' + context.payload.ref,
+      color: '30b227'
+    });
+
+    context.github.issues.createLabel(label);
+  });
 
   // app.on('issues.labeled', async ({ issue, label }) => {
   //   app.log(label.name + ' label added to ' + issue.id + '(' + issue.title + ')')
